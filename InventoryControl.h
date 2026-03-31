4cf8836673a3a1fef1ce64ae86223884cc47a64e
takeshita-2473 <ryuichiro_takeshita@aiphone.co.jp>
2026-03-31 14:01:37 +0900

    Update comments for inventory control functions

--- InventoryControl.h
+++ InventoryControl.h
@@ -12,16 +12,16 @@
  * プロトタイプ宣言
  */
 
- /* 飲み物 */
+ /* 飲み物を追加する関数*/
 void inv_add( DRINK drink );
 //void inv_add( enum DrinkKind type, const char* goodsName, int price, const char* note );
- /* 販売品リスト */
+ /* 販売品リスト追加する関数*/
 void inv_dispGoodsList( void );
- /* 在庫リスト */
+ /* 在庫リスト追加する関数 */
 void inv_dispStockList( void );
- /* お金とグッズの定義 */
+ /* お金とグッズの定義追加する関数 */
 int  inv_buying( int money, int goodsNo );
- /* 在庫数の設定 */
+ /* 在庫数の設定追加する関数 */
 void inv_setInitStock( int stockNum );
 
 

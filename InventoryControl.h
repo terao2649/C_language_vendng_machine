baadd76b70708212a8a98207653129561937cc34
takeshita-2473 <ryuichiro_takeshita@aiphone.co.jp>
2026-03-31 11:39:42 +0900

    ヘッダー追加”

--- InventoryControl.h
+++ InventoryControl.h
@@ -12,12 +12,16 @@
  * プロトタイプ宣言
  */
 
- /* InventoryControl */
+ /* 飲み物 */
 void inv_add( DRINK drink );
 //void inv_add( enum DrinkKind type, const char* goodsName, int price, const char* note );
+ /* 販売品リスト */
 void inv_dispGoodsList( void );
+ /* 在庫リスト */
 void inv_dispStockList( void );
+ /* お金とグッズの定義 */
 int  inv_buying( int money, int goodsNo );
+ /* 在庫数の設定 */
 void inv_setInitStock( int stockNum );
 
 

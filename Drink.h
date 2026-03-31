71d43e915ee387f8e9f79f098bf0cc8465100be2
xxnakamura <yuuka_nakamura@aiphone.co.jp>
2026-03-31 13:41:36 +0900

    Drink.hを修正

--- Drink.h
+++ Drink.h
@@ -1,5 +1,8 @@
 /**
  *    Drink.h
+ *	ドリンクについての定義
+ *
+ *	nakamura
  */
 #ifndef __DRINK_H__
 #define __DRINK_H__
@@ -7,14 +10,14 @@
 enum DrinkKind { COFFEE, WATER, TEA };
 
 typedef struct {
-	enum DrinkKind	type;	// 種類（COFFEE, WATER, TEA）
-	char		name[30];	// 名前
-	char		maker[30];	// メーカー
-	int			price;		// 値段
-	int			cold;	    // 冷温（1:Cold,0:Hot）
-	int			qty;		// 水量（ml）
-	int			stock;		// 在庫
-	char		note[30];	// 特記事項
+	enum DrinkKind	type;			// 種類（COFFEE, WATER, TEA）
+	char		name[30];			// 名前
+	char		maker[30];			// メーカー
+	int			price;				// 値段
+	int			cold_or_hot;	    // 冷温（1:Cold,0:Hot）
+	int			capacity;			// 容量（ml）
+	int			stock;				// 在庫
+	char		note[30];			// 特記事項
 }DRINK;
 
 #endif
